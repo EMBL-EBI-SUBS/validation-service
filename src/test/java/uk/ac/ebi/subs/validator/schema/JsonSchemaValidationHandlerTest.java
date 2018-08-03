88af2d9ed1c2f3fe0fc896408ae60e71ac6dd8c2
kalaij <1823024+kalaij@users.noreply.github.com>
2018-08-03 19:51:30 +0100

    study json schema handler test added

--- src/test/java/uk/ac/ebi/subs/validator/schema/JsonSchemaValidationHandlerTest.java
+++ src/test/java/uk/ac/ebi/subs/validator/schema/JsonSchemaValidationHandlerTest.java
@@ -12,6 +12,7 @@ import uk.ac.ebi.subs.data.component.StudyDataType;
 import uk.ac.ebi.subs.validator.coordinator.MesssageEnvelopeTestHelper;
 import uk.ac.ebi.subs.validator.data.SampleValidationMessageEnvelope;
 import uk.ac.ebi.subs.validator.data.SingleValidationResultsEnvelope;
+import uk.ac.ebi.subs.validator.data.StudyValidationMessageEnvelope;
 import uk.ac.ebi.subs.validator.data.structures.SingleValidationResultStatus;
 
 import static org.junit.Assert.*;
@@ -41,6 +42,12 @@ public class JsonSchemaValidationHandlerTest {
         jsonSchemaValidationHandler = new JsonSchemaValidationHandler(jsonSchemaValidationService, schemaService);
         jsonSchemaValidationHandler.setMlSampleSchemaUrl("https://raw.githubusercontent.com/EMBL-EBI-SUBS/validation-schemas/master/sample/ml-sample-schema.json");
         jsonSchemaValidationHandler.setSampleSchemaUrl("https://raw.githubusercontent.com/EMBL-EBI-SUBS/validation-schemas/master/sample/sample-schema.json");
+
+        jsonSchemaValidationHandler.setAssaySchemaUrl("https://raw.githubusercontent.com/EMBL-EBI-SUBS/validation-schemas/master/assay/assay-schema.json");
+        jsonSchemaValidationHandler.setMlSampleSchemaUrl("https://raw.githubusercontent.com/EMBL-EBI-SUBS/validation-schemas/master/assay/ml-assay-schema.json");
+
+        jsonSchemaValidationHandler.setStudySchemaUrl("https://raw.githubusercontent.com/EMBL-EBI-SUBS/validation-schemas/master/study/study-schema.json");
+        jsonSchemaValidationHandler.setMlStudySchemaUrl("https://raw.githubusercontent.com/EMBL-EBI-SUBS/validation-schemas/master/study/ml-study-schema.json");
     }
 
     @Test
@@ -57,5 +64,20 @@ public class JsonSchemaValidationHandlerTest {
         singleValidationResultsEnvelope = jsonSchemaValidationHandler.handleSampleValidation(sampleValidationEnvelope);
         assertEquals(singleValidationResultsEnvelope.getSingleValidationResults().size(), 1);
         assertEquals(singleValidationResultsEnvelope.getSingleValidationResults().get(0).getValidationStatus(), SingleValidationResultStatus.Pass);
+    }
+
+    @Test
+    public void handleStudyValidation() {
+        StudyValidationMessageEnvelope studyValidationMessageEnvelope = MesssageEnvelopeTestHelper.getStudyValidationMessageEnvelope();
+        SingleValidationResultsEnvelope singleValidationResultsEnvelope = jsonSchemaValidationHandler.handleStudyValidation(studyValidationMessageEnvelope);
+
+        assertEquals(singleValidationResultsEnvelope.getSingleValidationResults().size(), 7);
+        assertEquals(singleValidationResultsEnvelope.getSingleValidationResults().get(1).getMessage(),".attributes.factors error(s): should have required property 'factors'.");
+        assertEquals(singleValidationResultsEnvelope.getSingleValidationResults().get(2).getMessage(),".attributes.studyDesignDescriptors error(s): should have required property 'studyDesignDescriptors'.");
+
+        studyValidationMessageEnvelope.getEntityToValidate().setStudyType(StudyDataType.Proteomics);
+        singleValidationResultsEnvelope = jsonSchemaValidationHandler.handleStudyValidation(studyValidationMessageEnvelope);
+        assertEquals(singleValidationResultsEnvelope.getSingleValidationResults().size(), 4);
+
     }
 }
