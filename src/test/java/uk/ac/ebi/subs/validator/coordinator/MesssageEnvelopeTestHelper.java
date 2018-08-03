d612d862f3b093e6c2603dc30afd789caae35f23
kalaij <1823024+kalaij@users.noreply.github.com>
2018-08-03 18:24:53 +0100

    More utility methods added for validation tests

--- src/test/java/uk/ac/ebi/subs/validator/coordinator/MesssageEnvelopeTestHelper.java
+++ src/test/java/uk/ac/ebi/subs/validator/coordinator/MesssageEnvelopeTestHelper.java
@@ -1,9 +1,6 @@
 package uk.ac.ebi.subs.validator.coordinator;
 
-import uk.ac.ebi.subs.data.component.AbstractSubsRef;
-import uk.ac.ebi.subs.data.component.ProtocolRef;
-import uk.ac.ebi.subs.data.component.StudyDataType;
-import uk.ac.ebi.subs.data.component.Team;
+import uk.ac.ebi.subs.data.component.*;
 import uk.ac.ebi.subs.data.status.SubmissionStatusEnum;
 import uk.ac.ebi.subs.repository.model.Protocol;
 import uk.ac.ebi.subs.repository.model.Sample;
@@ -15,14 +12,23 @@ import uk.ac.ebi.subs.repository.repos.status.SubmissionStatusRepository;
 import uk.ac.ebi.subs.repository.repos.submittables.ProtocolRepository;
 import uk.ac.ebi.subs.repository.repos.submittables.SampleRepository;
 import uk.ac.ebi.subs.repository.repos.submittables.StudyRepository;
+import uk.ac.ebi.subs.validator.data.SampleValidationMessageEnvelope;
+import uk.ac.ebi.subs.validator.model.Submittable;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.*;
 
 public class MesssageEnvelopeTestHelper {
 
     static Submission saveNewSubmission(SubmissionStatusRepository submissionStatusRepository, SubmissionRepository submissionRepository, Team team) {
+        Submission submission = createNewSubmission(team);
+        submissionStatusRepository.insert(submission.getSubmissionStatus());
+        submissionRepository.save(submission);
+        return submission;
+    }
+
+    static Submission createNewSubmission(Team team) {
         Submission submssion = new Submission();
         submssion.setId(UUID.randomUUID().toString());
 
@@ -30,20 +36,17 @@ public class MesssageEnvelopeTestHelper {
 
         submssion.setSubmissionStatus(new SubmissionStatus(SubmissionStatusEnum.Draft));
         submssion.getSubmissionStatus().setTeam(team);
-
-        submissionStatusRepository.insert(submssion.getSubmissionStatus());
-        submissionRepository.save(submssion);
         return submssion;
     }
 
-    static Team createTeam () {
+    static Team createTeam() {
         Team team = new Team();
         team.setName(UUID.randomUUID().toString());
         return team;
     }
 
-    static List<Sample> createAndSaveSamples (SampleRepository sampleRepository, Submission submission, Team team, int sampleNumber) {
-        List<Sample> sampleList = createSamples(submission,team,sampleNumber);
+    static List<Sample> createAndSaveSamples(SampleRepository sampleRepository, Submission submission, Team team, int sampleNumber) {
+        List<Sample> sampleList = createSamples(submission, team, sampleNumber);
 
         for (Sample sample : sampleList) {
             sampleRepository.save(sample);
@@ -52,9 +55,9 @@ public class MesssageEnvelopeTestHelper {
         return sampleList;
     }
 
-    static List<Sample> createSamples (Submission submission, Team team, int sampleNumber) {
+    static List<Sample> createSamples(Submission submission, Team team, int sampleNumber) {
         List<Sample> sampleList = new ArrayList<>(sampleNumber);
-        for (int i = 0; i < sampleNumber; i++ ) {
+        for (int i = 0; i < sampleNumber; i++) {
             Sample sample = new Sample();
             sample.setTeam(team);
             String alias = UUID.randomUUID().toString();
@@ -62,14 +65,42 @@ public class MesssageEnvelopeTestHelper {
             sample.setAlias(alias);
             sample.setAccession(accession);
             sample.setSubmission(submission);
+            sample.setAttributes(generateUsiAttributes());
+            sample.setTaxonId(10090L);
+            sample.setReleaseDate(LocalDate.now());
             sampleList.add(sample);
         }
         return sampleList;
     }
 
-    static List<Protocol> createProtocols (Submission submission, Team team, int protocolNumber) {
+    static Map<String, Collection<Attribute>> generateUsiAttributes() {
+        Map<String, Collection<Attribute>> usiAttributes = new HashMap<>();
+
+        Attribute usiAttribute_1 = new Attribute();
+        usiAttribute_1.setValue("1.5");
+        Term term = new Term();
+        term.setUrl("http://purl.obolibrary.org/obo/UO_0000036");
+        usiAttribute_1.setTerms(Arrays.asList(term));
+        usiAttribute_1.setUnits("year");
+        usiAttributes.put("age", Arrays.asList(usiAttribute_1));
+
+        Attribute usiAttribute_2 = new Attribute();
+        usiAttribute_2.setValue(Instant.now().toString());
+        usiAttributes.put("update", Arrays.asList(usiAttribute_2));
+
+        Attribute usiAttribute_3 = new Attribute();
+        usiAttribute_3.setValue("mouse");
+        Term t = new Term();
+        t.setUrl("http://purl.obolibrary.org/obo/NCBITaxon_10090");
+        usiAttribute_3.setTerms(Arrays.asList(t));
+        usiAttributes.put("synonym", Arrays.asList(usiAttribute_3));
+
+        return usiAttributes;
+    }
+
+    static List<Protocol> createProtocols(Submission submission, Team team, int protocolNumber) {
         List<Protocol> protocols = new ArrayList<>(protocolNumber);
-        for (int i = 0; i < protocolNumber; i++ ) {
+        for (int i = 0; i < protocolNumber; i++) {
             Protocol protocol = new Protocol();
             protocol.setAccession(UUID.randomUUID().toString());
             protocol.setAlias(UUID.randomUUID().toString());
@@ -82,15 +113,20 @@ public class MesssageEnvelopeTestHelper {
         return protocols;
     }
 
-    static List<ProtocolRef> createProtocolRefs(List<Protocol> protocols){
+    static List<ProtocolRef> createProtocolRefs(List<Protocol> protocols) {
         List<ProtocolRef> refs = new ArrayList<>();
-        for(Protocol protocol : protocols){
+        for (Protocol protocol : protocols) {
             refs.add((ProtocolRef) protocol.asRef());
         }
         return refs;
     }
 
-    public static Study createAndSaveStudy (StudyRepository studyRepository, Submission submission, Team team) {
+    public static Study createAndSaveStudy(StudyRepository studyRepository, Submission submission, Team team) {
+        Study study = createStudy(submission, team);
+        return studyRepository.save(study);
+    }
+
+    public static Study createStudy(Submission submission, Team team) {
         Study study = new Study();
         study.setTeam(team);
         String projectAccession = UUID.randomUUID().toString();
@@ -99,12 +135,35 @@ public class MesssageEnvelopeTestHelper {
         study.setAccession(projectAccession);
         study.setSubmission(submission);
         study.setStudyType(StudyDataType.Metabolomics_LCMS);
-        return studyRepository.save(study);
+        return study;
     }
 
 
     public static List<Protocol> createAndSaveProtocols(ProtocolRepository protocolRepository, Submission submission, Team team) {
-        List<Protocol> protocols = MesssageEnvelopeTestHelper.createProtocols(submission,team, 3);
+        List<Protocol> protocols = MesssageEnvelopeTestHelper.createProtocols(submission, team, 3);
         return protocolRepository.save(protocols);
+    }
+
+    public static SampleValidationMessageEnvelope getSampleValidationEnvelope() {
+        SampleValidationMessageEnvelope sampleValidationMessageEnvelope = new SampleValidationMessageEnvelope();
+        Team team = createTeam();
+        Submission submission = createNewSubmission(team);
+
+        sampleValidationMessageEnvelope.setSubmissionId(submission.getId());
+        sampleValidationMessageEnvelope.setValidationResultUUID(UUID.randomUUID().toString());
+
+        List<Sample> samples = createSamples(submission, team, 3);
+        sampleValidationMessageEnvelope.setEntityToValidate(samples.get(0));
+
+        sampleValidationMessageEnvelope.setSampleList(getSubmittableSamples(samples, submission.getId()));
+        return sampleValidationMessageEnvelope;
+    }
+
+    static List<uk.ac.ebi.subs.validator.model.Submittable<uk.ac.ebi.subs.data.submittable.Sample>> getSubmittableSamples(List<Sample> samples, String submissionID) {
+        List<uk.ac.ebi.subs.validator.model.Submittable<uk.ac.ebi.subs.data.submittable.Sample>> submittableSamples = new ArrayList<>();
+        for (Sample sample : samples) {
+            submittableSamples.add(new Submittable<uk.ac.ebi.subs.data.submittable.Sample>(sample, submissionID));
+        }
+        return submittableSamples;
     }
 }
