68a0ada05b44c3c8878e2b2534e3d4afa6e7be7c
David Richardson <davidr@ebi.ac.uk>
2018-09-03 15:46:55 +0100

    use data type/checklist schema for validation instead of hard coding

--- src/test/java/uk/ac/ebi/subs/validator/schema/JsonSchemaValidationHandlerTest.java
+++ src/test/java/uk/ac/ebi/subs/validator/schema/JsonSchemaValidationHandlerTest.java
@@ -1,20 +1,30 @@
 package uk.ac.ebi.subs.validator.schema;
 
-import org.apache.http.HttpStatus;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import org.mockito.Mockito;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.DefaultResponseErrorHandler;
-import org.springframework.web.client.RestTemplate;
-import uk.ac.ebi.subs.data.component.StudyDataType;
+import uk.ac.ebi.subs.repository.model.Checklist;
+import uk.ac.ebi.subs.repository.model.DataType;
+import uk.ac.ebi.subs.repository.model.StoredSubmittable;
+import uk.ac.ebi.subs.repository.model.Study;
+import uk.ac.ebi.subs.repository.repos.submittables.StudyRepository;
+import uk.ac.ebi.subs.repository.repos.submittables.SubmittableRepository;
 import uk.ac.ebi.subs.validator.coordinator.MesssageEnvelopeTestHelper;
-import uk.ac.ebi.subs.validator.data.AssayValidationMessageEnvelope;
 import uk.ac.ebi.subs.validator.data.SingleValidationResultsEnvelope;
 import uk.ac.ebi.subs.validator.data.StudyValidationMessageEnvelope;
+import uk.ac.ebi.subs.validator.schema.model.JsonSchemaValidationError;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 
 @RunWith(SpringRunner.class)
@@ -23,58 +33,84 @@ public class JsonSchemaValidationHandlerTest {
     JsonSchemaValidationHandler jsonSchemaValidationHandler;
     JsonSchemaValidationService jsonSchemaValidationService;
 
-    private RestTemplate restTemplate = new RestTemplate();
-    private SchemaService schemaService;
+    StudyRepository studyRepository;
+
+    StudyValidationMessageEnvelope studyValidationMessageEnvelope;
+
+    JsonSchemaValidationError error = new JsonSchemaValidationError(Arrays.asList("fake error message"), "/a/fake/path");
 
     @Before
     public void setUp() {
-        restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
-            protected boolean hasError(HttpStatus statusCode) {
-                return false;
-            }
-        });
+        studyValidationMessageEnvelope = MesssageEnvelopeTestHelper.getStudyValidationMessageEnvelope();
 
-        schemaService = new SchemaService(restTemplate);
 
-        jsonSchemaValidationService = new JsonSchemaValidationService(restTemplate);
-        jsonSchemaValidationService.setJsonSchemaValidator("https://subs-json-schema-validator.herokuapp.com/validate");
+        studyRepository = Mockito.mock(StudyRepository.class);
 
-        jsonSchemaValidationHandler = new JsonSchemaValidationHandler(jsonSchemaValidationService, schemaService);
-        
-        jsonSchemaValidationHandler.setAssaySchemaUrl("https://raw.githubusercontent.com/EMBL-EBI-SUBS/validation-schemas/master/assay/assay-schema.json");
-        jsonSchemaValidationHandler.setMlAssaySchemaUrl("https://raw.githubusercontent.com/EMBL-EBI-SUBS/validation-schemas/master/assay/ml-assay-schema.json");
+        Map<Class<? extends StoredSubmittable>, SubmittableRepository<? extends StoredSubmittable>> submittableRepositoryMap = new HashMap<>();
+        submittableRepositoryMap.put(Study.class, studyRepository);
 
-        jsonSchemaValidationHandler.setStudySchemaUrl("https://raw.githubusercontent.com/EMBL-EBI-SUBS/validation-schemas/master/study/study-schema.json");
-        jsonSchemaValidationHandler.setMlStudySchemaUrl("https://raw.githubusercontent.com/EMBL-EBI-SUBS/validation-schemas/master/study/ml-study-schema.json");
+        DataType dataType = new DataType();
+        Checklist checklist = new Checklist();
+
+        dataType.setValidationSchema(jsonStringToNode("{\"schema\": \"foo\"}"));
+        checklist.setValidationSchema(jsonStringToNode("{\"schema\": \"bar\"}"));
+
+
+        Study study = new Study();
+        study.setDataType(dataType);
+        study.setChecklist(checklist);
+
+
+        Mockito.when(studyRepository.findOne(studyValidationMessageEnvelope.getEntityToValidate().getId())).thenReturn(
+                study
+        );
+
+
+        jsonSchemaValidationService = Mockito.mock(JsonSchemaValidationService.class);
+        jsonSchemaValidationHandler = new JsonSchemaValidationHandler(submittableRepositoryMap, jsonSchemaValidationService);
+
     }
+
 
     @Test
     public void handleStudyValidation() {
-        StudyValidationMessageEnvelope studyValidationMessageEnvelope = MesssageEnvelopeTestHelper.getStudyValidationMessageEnvelope();
-        SingleValidationResultsEnvelope singleValidationResultsEnvelope = jsonSchemaValidationHandler.handleStudyValidation(studyValidationMessageEnvelope);
 
-        assertEquals(singleValidationResultsEnvelope.getSingleValidationResults().size(), 7);
-        assertEquals(singleValidationResultsEnvelope.getSingleValidationResults().get(1).getMessage(), ".attributes.factors error(s): should have required property 'factors'.");
-        assertEquals(singleValidationResultsEnvelope.getSingleValidationResults().get(2).getMessage(), ".attributes.studyDesignDescriptors error(s): should have required property 'studyDesignDescriptors'.");
+        JsonNode expectedDtSchema = jsonStringToNode("{\"$schema\": \"foo\"}");
+        JsonNode expectedClSchema = jsonStringToNode("{\"$schema\": \"bar\"}");
 
-        studyValidationMessageEnvelope.getEntityToValidate().setStudyType(StudyDataType.Proteomics);
-        singleValidationResultsEnvelope = jsonSchemaValidationHandler.handleStudyValidation(studyValidationMessageEnvelope);
-        assertEquals(singleValidationResultsEnvelope.getSingleValidationResults().size(), 4);
+
+        Mockito.when(jsonSchemaValidationService.validate(Mockito.eq(expectedDtSchema), Mockito.any())).thenReturn(
+                Arrays.asList(
+                        error
+                )
+        );
+        Mockito.when(jsonSchemaValidationService.validate(Mockito.eq(expectedClSchema), Mockito.any())).thenReturn(
+                Arrays.asList(
+                        error
+                )
+        );
+
+
+        SingleValidationResultsEnvelope singleValidationResultsEnvelope = jsonSchemaValidationHandler.handleSubmittableValidation(studyValidationMessageEnvelope);
+
+        Mockito.verify(jsonSchemaValidationService,Mockito.times(1)).validate(Mockito.eq(expectedDtSchema),Mockito.any());
+
+        Mockito.verify(jsonSchemaValidationService,Mockito.times(1)).validate(Mockito.eq(expectedClSchema),Mockito.any());
+
+
+        singleValidationResultsEnvelope.getSingleValidationResults();
+
+
+        assertEquals(singleValidationResultsEnvelope.getSingleValidationResults().size(), 2);
 
     }
 
-
-    @Test
-    public void handleAssayValidation() {
-        AssayValidationMessageEnvelope assayValidationMessageEnvelope = MesssageEnvelopeTestHelper.getAssayValidationMessageEnvelope();
-        SingleValidationResultsEnvelope singleValidationResultsEnvelope = jsonSchemaValidationHandler.handleAssayValidation(assayValidationMessageEnvelope);
-
-        assertEquals(singleValidationResultsEnvelope.getSingleValidationResults().size(), 7);
-        assertEquals(singleValidationResultsEnvelope.getSingleValidationResults().get(0).getMessage(), ".attributes.technologyType error(s): should have required property 'technologyType'.");
-
-        assayValidationMessageEnvelope.getStudy().getBaseSubmittable().setStudyType(StudyDataType.Proteomics);
-        singleValidationResultsEnvelope = jsonSchemaValidationHandler.handleAssayValidation(assayValidationMessageEnvelope);
-        assertEquals(singleValidationResultsEnvelope.getSingleValidationResults().size(), 4);
-
+    private ObjectNode jsonStringToNode(String str) {
+        try {
+            return new ObjectMapper().readValue(str, ObjectNode.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
