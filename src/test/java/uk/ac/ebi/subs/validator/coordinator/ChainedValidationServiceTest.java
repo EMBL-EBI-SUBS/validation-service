6cc4f8ed9883fa833b6aa5fb85a856c87e62981b
David Richardson <davidr@ebi.ac.uk>
2018-10-22 11:27:18 +0100

    make chained validation targeted

--- src/test/java/uk/ac/ebi/subs/validator/coordinator/ChainedValidationServiceTest.java
+++ src/test/java/uk/ac/ebi/subs/validator/coordinator/ChainedValidationServiceTest.java
@@ -4,153 +4,156 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import uk.ac.ebi.subs.data.component.AbstractSubsRef;
+import uk.ac.ebi.subs.data.component.AnalysisRef;
+import uk.ac.ebi.subs.data.component.AssayRef;
+import uk.ac.ebi.subs.data.component.EgaDacPolicyRef;
+import uk.ac.ebi.subs.data.component.EgaDacRef;
+import uk.ac.ebi.subs.data.component.EgaDatasetRef;
+import uk.ac.ebi.subs.data.component.ProjectRef;
+import uk.ac.ebi.subs.data.component.ProtocolRef;
+import uk.ac.ebi.subs.data.component.SampleGroupRef;
+import uk.ac.ebi.subs.data.component.SampleRef;
+import uk.ac.ebi.subs.data.component.StudyRef;
 import uk.ac.ebi.subs.data.component.Team;
+import uk.ac.ebi.subs.data.submittable.Analysis;
+import uk.ac.ebi.subs.data.submittable.EgaDac;
+import uk.ac.ebi.subs.data.submittable.EgaDataset;
+import uk.ac.ebi.subs.data.submittable.Project;
+import uk.ac.ebi.subs.data.submittable.SampleGroup;
+import uk.ac.ebi.subs.data.submittable.Submittable;
+import uk.ac.ebi.subs.repository.model.Assay;
 import uk.ac.ebi.subs.repository.model.DataType;
+import uk.ac.ebi.subs.repository.model.EgaDacPolicy;
+import uk.ac.ebi.subs.repository.model.Protocol;
 import uk.ac.ebi.subs.repository.model.Sample;
-import uk.ac.ebi.subs.repository.model.StoredSubmittable;
 import uk.ac.ebi.subs.repository.model.Study;
 import uk.ac.ebi.subs.repository.model.Submission;
-import uk.ac.ebi.subs.repository.repos.DataTypeRepository;
-import uk.ac.ebi.subs.repository.repos.SubmissionRepository;
-import uk.ac.ebi.subs.repository.repos.submittables.SampleRepository;
+import uk.ac.ebi.subs.repository.repos.submittables.AssayRepository;
 import uk.ac.ebi.subs.repository.repos.submittables.StudyRepository;
-import uk.ac.ebi.subs.validator.TestUtils;
-import uk.ac.ebi.subs.validator.data.ValidationResult;
-import uk.ac.ebi.subs.validator.data.structures.GlobalValidationStatus;
-import uk.ac.ebi.subs.validator.repository.ValidationResultRepository;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import java.util.stream.Stream;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class ChainedValidationServiceTest {
 
-    @Autowired
+    //service under test
     private ChainedValidationService service;
-    @MockBean
+
+    //mock beans
     private SubmittableHandler submittableHandler;
-
-    @Autowired
-    private SubmissionRepository submissionRepository;
-    @Autowired
-    private SampleRepository sampleRepository;
-    @Autowired
     private StudyRepository studyRepository;
-    @Autowired
-    private ValidationResultRepository validationResultRepository;
-    @Autowired
-    private DataTypeRepository dataTypeRepository;
+    private AssayRepository assayRepository;
 
 
-    private Submission submission;
+    //test data
     private Study study;
+    private Assay assay;
 
-    private DataType dataType;
-
+    private String submissionId = "submission";
+    private String dataTypeId = "dt1";
 
     @Before
     public void setUp() {
-        clearDB();
+        submittableHandler = Mockito.mock(SubmittableHandler.class);
+        studyRepository = Mockito.mock(StudyRepository.class);
+        assayRepository = Mockito.mock(AssayRepository.class);
 
-        dataType = TestUtils.createDataType("aDataType", Collections.emptyList())      ;
-        dataTypeRepository.insert(
-                dataType
+        service = new ChainedValidationService(Arrays.asList(assayRepository, studyRepository), submittableHandler);
+
+        DataType dataType = new DataType();
+        dataType.setId(dataTypeId);
+
+        Submission submission = new Submission();
+        submission.setId(submissionId);
+
+        assay = new Assay();
+        assay.setDataType(dataType);
+        assay.setSubmission(submission);
+
+        study = new Study();
+        fillInNames(study);
+        study.setSubmission(submission);
+    }
+
+    @Test
+    public void test_chaining_validation() {
+        AbstractSubsRef ref = study.asRef();
+
+        Mockito.when(
+                assayRepository.findBySubmissionIdAndReference(submissionId, ref)
+        ).thenReturn(Collections.singletonList(assay));
+
+        Mockito.when(
+                studyRepository.findBySubmissionIdAndReference(submissionId, ref)
+        ).thenReturn(Collections.emptyList());
+
+        service.triggerChainedValidation(study, submissionId);
+
+        Mockito.verify(submittableHandler).handleSubmittable(assay, submissionId, dataTypeId, null);
+
+    }
+
+    @Test
+    public void test_ref_generation() {
+        List<Submittable> submittables = Arrays.asList(
+                new Analysis(),
+                new Assay(),
+                new EgaDac(),
+                new EgaDacPolicy(),
+                new EgaDataset(),
+                new Project(),
+                new Protocol(),
+                new Sample(),
+                new SampleGroup(),
+                new Study()
+        );
+        submittables.forEach(this::fillInNames);
+
+        List<AbstractSubsRef> expectedRefs = Arrays.asList(
+                new AnalysisRef(),
+                new AssayRef(),
+                new EgaDacRef(),
+                new EgaDacPolicyRef(),
+                new EgaDatasetRef(),
+                new ProjectRef(),
+                new ProtocolRef(),
+                new SampleRef(),
+                new SampleGroupRef(),
+                new StudyRef()
+        );
+        expectedRefs.forEach(this::fillInNames);
+
+        List<AbstractSubsRef> actualRefs = submittables.stream()
+                .map(submittable -> service.submittableToRef(submittable))
+                .collect(Collectors.toList());
+
+        Assert.assertEquals(
+                expectedRefs,
+                actualRefs
         );
 
-        submission = createSubmission();
-        submissionRepository.insert(submission);
-        createSamples();
-        study = createStudy();
     }
 
-    @Test
-    public void findSubmittablesInSubmissionTest() {
-        Stream<? extends StoredSubmittable> submittablesStream = service.streamSubmittablesInSubmission(submission.getId());
-
-        Map<String, List<StoredSubmittable>> submittablesInSubmission = groupSubmittablesByClassName(submittablesStream);
-
-        Assert.assertEquals(3, submittablesInSubmission.get("Sample").size());
-        Assert.assertFalse(submittablesInSubmission.containsKey("Assay"));
-        Assert.assertEquals(1, submittablesInSubmission.get("Study").size());
-        Assert.assertFalse(submittablesInSubmission.containsKey("AssayData"));
+    private void fillInNames(Submittable submittable) {
+        submittable.setAlias("foo");
+        submittable.setTeam(Team.build("bar"));
+        submittable.setAccession("X1234");
     }
 
-    private Map<String, List<StoredSubmittable>> groupSubmittablesByClassName(Stream<? extends StoredSubmittable> submittablesStream) {
-        Map<String, List<StoredSubmittable>> submittablesInSubmission = new HashMap<>();
-
-        submittablesStream.forEach(submittable -> {
-            String key = submittable.getClass().getSimpleName();
-
-            if (!submittablesInSubmission.containsKey(key)) {
-                submittablesInSubmission.put(key, new ArrayList<>());
-            }
-            submittablesInSubmission.get(key).add(submittable);
-        });
-        return submittablesInSubmission;
+    private void fillInNames(AbstractSubsRef ref) {
+        ref.setAlias("foo");
+        ref.setTeam("bar");
+        ref.setAccession("X1234");
     }
 
-    @Test
-    public void filterOutTriggerSubmittableTest() {
-        Stream<? extends StoredSubmittable> submittablesStream = service.streamSubmittablesInSubmissionExceptTriggerSubmittable(study, submission.getId());
-
-        Map<String, List<StoredSubmittable>> submittablesInSubmission = groupSubmittablesByClassName(submittablesStream);
-
-        Assert.assertFalse(submittablesInSubmission.containsKey("Study"));
-
-        Assert.assertEquals(3, submittablesInSubmission.get("Sample").size());
-        Assert.assertFalse(submittablesInSubmission.containsKey("Assay"));
-        Assert.assertFalse(submittablesInSubmission.containsKey("AssayData"));
-    }
-
-    @Test
-    public void triggerChainedValidationTest() {
-        service.triggerChainedValidation(study, submission.getId());
-        verify(submittableHandler, times(3)).handleSubmittable(any(), any(), any(), any());
-    }
-
-    private Submission createSubmission() {
-        Submission sub = new Submission();
-        sub.setId(UUID.randomUUID().toString());
-        Team d = generateTestTeam();
-        sub.setTeam(d);
-        return sub;
-    }
-
-    private void createSamples() {
-        for (int i = 0; i < 3; i++) {
-            Sample sample = new Sample();
-            sample.setId(UUID.randomUUID().toString());
-            sample.setSubmission(submission);
-            sample.setDataType(dataType);
-            sampleRepository.insert(sample);
-            generateValidationResult(sample.getId());
-        }
-    }
-
-    private Study createStudy() {
-        Study study = new Study();
-        study.setId(UUID.randomUUID().toString());
-        study.setSubmission(submission);
-        study.setDataType(dataType);
-        Team d = generateTestTeam();
-        study.setTeam(d);
-        generateValidationResult(study.getId());
-        return studyRepository.insert(study);
-    }
 
     private static Team generateTestTeam() {
         Team d = new Team();
@@ -158,20 +161,5 @@ public class ChainedValidationServiceTest {
         return d;
     }
 
-    private void generateValidationResult(String submittableId) {
-        ValidationResult validationResult = new ValidationResult();
-        validationResult.setUuid(UUID.randomUUID().toString());
-        validationResult.setEntityUuid(submittableId);
-        validationResult.setVersion(1);
-        validationResult.setSubmissionId(submission.getId());
-        validationResult.setValidationStatus(GlobalValidationStatus.Complete);
-        validationResultRepository.save(validationResult);
-    }
 
-    private void clearDB() {
-        submissionRepository.deleteAll();
-        sampleRepository.deleteAll();
-        studyRepository.deleteAll();
-        dataTypeRepository.deleteAll();
-    }
 }
